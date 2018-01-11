8f79b895d78fe4db21ac32a552e44bb1d27e8968
Mahokkit <ma.edward@hotmail.com>
2018-01-11 11:14:05 -0400

    implemented using block

--- ADONetFirstDemo/ADONetFirstDemo/DataReadExample.cs
+++ ADONetFirstDemo/ADONetFirstDemo/DataReadExample.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ADONetFirstDemo
@@ -13,21 +14,41 @@ namespace ADONetFirstDemo
         {
             string connectionString 
                 = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\NSCCStudent\Desktop\Northwind.mdf';Integrated Security=True;Connect Timeout=30";
-            
-            //Establish Connection
-            SqlConnection conn = new SqlConnection(connectionString);
-            conn.Open();
-            //Create command with the connection
-            SqlCommand cmd = new SqlCommand("SELECT * FROM products", conn);
-            
-            //Execute the command....reader because it is a select statement
-            SqlDataReader reader = cmd.ExecuteReader();
-            while(reader.Read())
-            {
-                Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
-            }
-            conn.Close();
 
+            //Establish Connection
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                //Create command with the connection
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM products", conn))
+                {
+                    //Execute the command....reader because it is a select statement
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("\t{0}\t{1}\t{2}", reader["ProductName"], reader["UnitPrice"], reader["UnitsInStock"]);
+
+                            Thread.Sleep(250);
+                        }
+                    }
+                }
+            }
+
+            //JAVA EQUIVALENT 
+            //try
+            //{
+            //    //open a db connection
+            //}
+            //catch(Exception e)
+            //{
+            //    //handle any errors
+            //}
+            //finally
+            //{
+            //    //close and dispose of the object
+            //}
             //keep the console windows open until user enter any key.
             Console.ReadKey();
         }
