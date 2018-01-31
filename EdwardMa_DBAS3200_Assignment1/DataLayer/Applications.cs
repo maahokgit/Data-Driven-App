89c92e9a985b8e48d5d87ca2923255e036119e05
Mahokkit <ma.edward@hotmail.com>
2018-01-31 18:39:47 -0400

    create InsertApp, DeleteApp method in Applications.cs library

--- EdwardMa_DBAS3200_Assignment1/DataLayer/Applications.cs
+++ EdwardMa_DBAS3200_Assignment1/DataLayer/Applications.cs
@@ -31,9 +31,69 @@ namespace DataLayer
                     }
                 }
             }
-
             return applications;
         }
+
+        /// <summary>
+        /// Delete an Application from DB using the name of Application
+        /// </summary>
+        /// <param name="appName"></param>
+        public void DeleteApp(string appName) //delete an application from DB
+        {
+            using (SqlConnection connection = DB.GetSqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"deleteApp";
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+                    SqlParameter p_appName = new SqlParameter("p_AppName", System.Data.SqlDbType.NVarChar, 40);
+                    p_appName.Value = appName;
+
+                    command.Parameters.Add(p_appName);
+
+                    command.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Insert a new Application, using Application Name, Version and Desc
+        /// </summary>
+        /// <param name="p_appName"></param>
+        /// <param name="p_appVersion"></param>
+        /// <param name="p_appDesc"></param>
+        public void InsertApp(string p_appName, string p_appVersion, string p_appDesc)
+        {
+            using (SqlConnection connection = DB.GetSqlConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"insertApp";
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    SqlParameter SQLP_AppName = new SqlParameter("p_AppName", System.Data.SqlDbType.NVarChar, 40);
+                    SQLP_AppName.Value = p_appName;
+                    command.Parameters.Add(SQLP_AppName);
+
+                    SqlParameter SQLP_AppVersion = new SqlParameter("p_AppVersion", System.Data.SqlDbType.NVarChar, 40);
+                    SQLP_AppVersion.Value = p_appVersion;
+                    command.Parameters.Add(SQLP_AppVersion);
+
+                    //used for concurrency check (manual)
+                    SqlParameter SQLP_AppDesc = new SqlParameter("p_AppDesc", System.Data.SqlDbType.NVarChar, 255);
+                    SQLP_AppDesc.Value = p_appDesc;
+                    command.Parameters.Add(SQLP_AppDesc);
+
+                    command.ExecuteNonQuery();
+
+                }
+            }
+        }
+
 
         public class Application
         {
