5d0b8ea62a8c44e4c6b1c5ec72d51aaff13070bc
Mahokkit <ma.edward@hotmail.com>
2018-02-01 12:09:48 -0400

    fixing Bug Procedures

--- EdwardMa_DBAS3200_Assignment1/BugTrackerDB/StoredProcedures/Bugs/getBug.sql
+++ EdwardMa_DBAS3200_Assignment1/BugTrackerDB/StoredProcedures/Bugs/getBug.sql
@@ -1,4 +1,32 @@
 ﻿CREATE PROCEDURE [dbo].[getBug]
 AS
-	SELECT * FROM Bugs b
+	SELECT 
+		b.BugID, 
+		b.AppID, 
+		b.UserID, 
+		bL.BugLogID, 
+		b.BugDate, 
+		b.BugDetails,
+		b.RepSteps,
+		b.FixDate,
+		bL.BugLogDate,
+		bL.StatusCodeID,
+		bL.BugLogDesc
+	FROM Bugs b
 	INNER JOIN BugLog bL ON bL.BugID = b.BugID
+
+	/*
+		BugID
+		AppID
+		UserID
+		BugLogID
+		BugDate
+		BugDetails
+		BugDesc
+		RepSteps
+		FixDate
+		BugLogDate
+		StatusCodeID
+		UserID
+		BugLogDesc
+	*/
