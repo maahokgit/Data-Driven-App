1f2f67cc0d68880c7985213cf7fe1fa67e907c23
Mahokkit <ma.edward@hotmail.com>
2018-02-19 13:40:39 -0400

    created Context class.

---
+++ EdwardMa_DBAS3200_Assignment2/AppDBDatalayer/AppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using AppDBDatalayer.Models;
+
+namespace AppDBDatalayer
+{
+    class AppDBContext : DbContext
+    {
+        public AppDBContext() : base("AppDB")
+        {
+            Database.SetInitializer(
+                new DropCreateDatabaseIfModelChanges<AppDBContext>()
+                );
+        }
+
+        //define the collections of object
+        //in the database....tables
+        public DbSet<CampusTable> Campus { get; set; }
+        public DbSet<ProgramTable> Program { get; set; }  
+        public DbSet<CampusProgramTable> CampusProgram { get; set; }
+        public DbSet<CitizenshipTable> Citizenship { get; set; }
+        public DbSet<CountryTable> Country { get; set; }
+        public DbSet<ProvinceStateTable> ProvinceState { get; set; }
+        public DbSet<ApplicantTable> Applicant { get; set; }
+        public DbSet<ApplicationTable> Application { get; set; }
+        public DbSet<ProgramChoiceTable> ProgramChoice { get; set; }
+    }
+}
