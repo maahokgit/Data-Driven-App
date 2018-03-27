3ae11d1b902e8e1fa784a4f1cc64e6d26904a585
Mahokkit <ma.edward@hotmail.com>
2018-03-27 09:43:17 -0300

    finished.

--- EdwardMa_DBAS3200_Assignment2/WebApplication1/Global.asax.cs
+++ EdwardMa_DBAS3200_Assignment2/WebApplication1/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
 
-namespace AppDBApi
+namespace WebApplication1
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
